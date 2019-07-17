20c8d5417a001402e0a2d6019cf713bb4a6e6ed7
Lukasz Ratajczak <miszeq12@gmail.com>
2019-07-17 18:53:58 +0200

    How many stairs will Suzuki climb in 20 years?

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,13 @@ public class Main {
 
     }
 
-    
+    public static long stairsIn20(int[][] stairs) {
+        long stairsIn1 = 0;
+        for (int i = 0; i < stairs.length; i++) {
+            for (int j = 0; j < stairs[i].length; j++) {
+                stairsIn1 += stairs[i][j];
+            }
+        }
+        return stairsIn1 * 20;
+    }
 }
